3fdffd1b7c580f3381cada948be162d908b47149
DeltaZulu <bbgun567@gmail.com>
2021-12-13 11:40:52 -0500

    Create challenge8.js

---
+++ challenge8.js
@@ -0,0 +1,11 @@
+/* function countActiveAstronauts
+   Description: A function declared to a constant variable that returns the number of astronauts in a roster array
+   Parameters:
+     roster (Array) - An array/list of astronauts
+   Return:
+     The number of astronauts in the roster list
+*/
+const countActiveAstronauts = (roster) => {
+  // Return roster list length
+  return roster.length;
+}
